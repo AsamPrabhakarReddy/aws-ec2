f06ccfe1f98e01103cba374a5b45317ef90cce70
ASAM PRABHAKAR REDDY <asamprabhakarbala@gmail.com>
2024-08-31 19:33:07 +0530

    adding build file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
   return (
     <div>
         <h1>Hello World</h1>
-        <p>This is deployed on AWS EC2 instance</p>
+        <p>This is deployed on AWS EC2 instance with CI/CD</p>
     </div>
   );
 }
